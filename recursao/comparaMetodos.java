3c6b880dddb55260d62a7127d50e963183d39c61
LucasKenz <lkmtada@gmail.com>
2024-04-23 11:04:20 -0300

    aula 16 lab quebra quick

--- recursao/comparaMetodos.java
+++ recursao/comparaMetodos.java
@@ -7,27 +7,34 @@ public class ComparaMetodos{
         MeuVetor v;
         System.out.println("digite o tamanho do vetor, 0 encerra: ");
         int tamanho = scanner.nextInt();
+        long ini, fim;
         while (tamanho >0){
             v = new MeuVetor(tamanho);
-            // v.preencheVetor();
-            // //System.out.println("vetor para o bubble: " + v);
-            // long ini = new Date().getTime();
-            // v.bubbleSort();
-            // long fim = new Date().getTime();
-            // //System.out.println("vetor ordenado pelo Bubble: " + v);
-            // System.out.println("bubble demorou: " + (fim-ini) + "ms ");
-            // // declara vetor, começa o tempo, sort, encerra tempo, printa v e tempo
 
-            // v.resetVetor();
-
-            //quicksort
             v.preencheVetor();
+            //System.out.println("vetor para o bubble: " + v);
+            ini = new Date().getTime();
+            v.bubbleSort();
+            fim = new Date().getTime();
+            //System.out.println("vetor ordenado pelo Bubble: " + v);
+            System.out.println("bubble demorou: " + (fim-ini) + "ms ");
+            // declara vetor, começa o tempo, sort, encerra tempo, printa v e tempo
+            
+            //quicksort
+            v.resetVetor();
+            v.preencheVetor();
+            
             //System.out.println("vetor para o quicksort" + v);
-            long ini = new Date().getTime();
+            ini = new Date().getTime();
             v.quicksort(0, tamanho-1);
-            long fim = new Date().getTime();
+            fim = new Date().getTime();
             //System.out.println("vetor ordenado pelo Quick: \n" + v);
-            //System.out.println("Quick Demorou: " + (fim-ini) + "ms");
+            System.out.println("Quick Demorou: " + (fim-ini) + "ms");
+
+            ini = new Date().getTime();
+            v.quicksort(0, tamanho-1);
+            fim = new Date().getTime();
+            System.out.println("Quick Demorou: " + (fim-ini) + "ms");
 
             System.out.println("digite o tamanho, 0 encerra: ");
             tamanho = scanner.nextInt();
