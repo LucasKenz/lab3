ca58d0aa0386ca2b3d414d89afaf14b6ba2b1c42
LucasKenz <lkmtada@gmail.com>
2024-05-15 10:54:59 -0300

    beecreowd 1162 feito

---
+++ beecrowd/bee1162/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         int N_Trains = scanner.nextInt();
+
+//         for (int times = 0; times < N_Trains; times++) {
+//             int N = scanner.nextInt();
+//             int[] L = new int[N];
+//             for (int i = 0; i < N; i++) {
+//                 L[i] = scanner.nextInt();
+//             }
+
+//             int swapps = 0;
+//             for (int i = 0; i < N; i++) {
+//                 for (int j = 0; j < N - 1; j++) {
+//                     if (L[j] > L[j + 1]) {
+//                         int temp = L[j];
+//                         L[j] = L[j + 1];
+//                         L[j + 1] = temp;
+//                         swapps++;
+//                     }
+//                 }
+//             }
+//             System.out.println("Optimal train swapping takes " + swapps + " swaps.");
+//         }
+//         scanner.close();
+//     }
+// }
+
+public class Main {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+    }
+}
+
+
+
